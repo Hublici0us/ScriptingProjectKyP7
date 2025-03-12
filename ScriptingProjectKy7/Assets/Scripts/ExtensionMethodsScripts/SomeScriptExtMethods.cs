95ac52f4449fca3ad43b2caa669629b93e165ee9
Jason Ky <744169@student.dusd.net>
2025-03-11 21:19:23 -0700

    ExtensionMethods

---
+++ ScriptingProjectKy7/Assets/Scripts/ExtensionMethodsScripts/SomeScriptExtMethods.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeScriptExtMethods : MonoBehaviour
+{
+    void Start()
+    {
+        transform.ResetTransformation();
+    }
+
+}
